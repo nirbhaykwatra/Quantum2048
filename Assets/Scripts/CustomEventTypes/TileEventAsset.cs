33baeb60a56bdc5a278ff588962ee48365bd13f3
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2025-05-06 17:34:35 -0700

    Create new tile event type

---
+++ Assets/Scripts/CustomEventTypes/TileEventAsset.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace GameEvents
+{
+    [CreateAssetMenu(menuName = "Events/Tile Event Asset")]
+    public class TileEventAsset : GameEventAsset<Tile> {}
+}
---
+++ Assets/Scripts/CustomEventTypes/TileEventCaller.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+namespace GameEvents
+{
+    public class TileEventCaller : GameEventCaller<Tile> {}
+}
