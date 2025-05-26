b93bd1aac229124deb4e64b91eebb70f32f8acab
Nirbhay Kwatra <53355072+nirbhaykwatra@users.noreply.github.com>
2025-05-26 16:35:47 -0700

    Create main menu with button prefabs

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log($"Loading Scene: {sceneName}");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+    }
+}
